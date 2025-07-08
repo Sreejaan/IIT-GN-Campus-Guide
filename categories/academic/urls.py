8d77d21ff18be11f4372df9bb0e19d13ab2a4cbf
Sreejan09 <sreejangupta9@gmail.com>
2025-07-08 21:51:00 +0530

    add academic details and modified css

--- categories/academic/urls.py
+++ categories/academic/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.academic_index, name='academic_index'),
-    path('<slug:slug>/', views.academic_detail, name='academic_detail'),
+    # path('<slug:slug>/', views.academic_detail, name='academic_detail'),
+    path('<slug:building_slug>/', views.academic_building, name='academic_building'),
 ]
--- categories/academic/views.py
+++ categories/academic/views.py
@@ -1,7 +1,28 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from core.models import Building, Location
+
+
 
 def academic_index(request):
-    return HttpResponse("Academic Category Home")
+    academic = Building.objects.filter(category='Academic')
+    return render(request, 'academic/index.html', {'academic': academic})
 
-def academic_detail(request, slug):
-    return HttpResponse(f"Details for academic location: {slug}")
+
+def academic_building(request, building_slug):
+    building = get_object_or_404(Building, slug=building_slug, category='Academic')
+    return render(request, 'academic/building.html', {'building': building})
+
+# def hostel_place_type(request, building_slug, place_type):
+#     building = get_object_or_404(Building, slug=building_slug)
+#     locations = Location.objects.filter(floor__building=building, type=place_type)
+#     return render(request, 'academic/place_type.html', {
+#         'building': building,
+#         'locations': locations,
+#         'place_type': place_type,
+#     })
+
+# def hostel_location_detail(request, building_slug, place_type, location_slug):
+#     location = get_object_or_404(Location, slug=location_slug, type=place_type, floor__building__slug=building_slug)
+#     return render(request, 'academic/location_detail.html', {
+#         'location': location
+#     })
